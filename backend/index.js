0bc10eaecd7b232704c3f235298f0845d2d4d299
Thiago Mariotto <thiagomariotto@outlook.com.br>
2022-08-30 19:01:34 -0300

    fix: add commentario

--- backend/index.js
+++ backend/index.js
@@ -7,15 +7,17 @@ const PORT = process.env.PORT || 3333;
 
 app.use(cors());
 app.get('/', async (_req, res) => {
-  try {
-    const [instructors] = await connection.execute(
-      'SELECT * FROM technology.language',
-    );
+	try {
+		const [instructors] = await connection.execute(
+			'SELECT * FROM technology.language',
+		);
 
-    return res.status(200).json(instructors);
-  } catch (e) {
-    return res.status(500).send(e);
-  }
+		// HI
+
+		return res.status(200).json(instructors);
+	} catch (e) {
+		return res.status(500).send(e);
+	}
 });
 
 app.listen(PORT);
